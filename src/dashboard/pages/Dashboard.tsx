4c39eed04fdf461b26c48a27373dc22adc60635a
WebDeveloperM <webdevelopermm1514@gmail.com>
2024-11-23 08:03:41 +0500

    new fixed run

--- src/dashboard/pages/Dashboard.tsx
+++ src/dashboard/pages/Dashboard.tsx
@@ -1,15 +1,9 @@
 import Layout from "@core/components/Layout";
 import { useState } from "react";
 import Main from "../components/Main";
-import { isAuthenticated } from "@users/utils/auth";
 
 export default function Dashboard() {
     const [open, setOpen] = useState(true);
-    // const navigate = useNavigate()
-
-    console.log(isAuthenticated());
-    
-    // if (!isAuthenticated()) navigate('/')
 
     return (
 
